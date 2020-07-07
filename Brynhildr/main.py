94d3b61fd7d5d55ed27d764348225c755c38db0c
vic-yush <66185358+vic-yush@users.noreply.github.com>
2020-07-07 11:49:24 -0400

    Add files via upload
    Added help and changelog functionality

--- Brynhildr/main.py
+++ Brynhildr/main.py
@@ -3,22 +3,15 @@ import discord
 import dateparser
 import datetime
 import pytz
-import requests
 
 client = discord.Client()
 
 ERRORMESSAGE = "Sorry, I couldn't understand that. Could you try again?"
-MENTIONS = ("hey bryn", "hey brynhildr", "hey brynhild", "hi bryn",
-            "hi brynhildr", "hi brynhild", "okay bryn", "okay brynhildr",
-            "okay brynhild")
 
 
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching,
-                                  name="the stars"))
 
 
 @client.event
@@ -26,14 +19,12 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith(MENTIONS) or client.user in \
+    if message.content.lower().startswith("Hey") or client.user in \
             message.mentions:
         if "remind me" in message.content.lower():
             await reminder(message.content.lower(), message)
         elif "remindme" in message.content.lower():
             await reminderstripped(message.content.lower(), message)
-        elif "lookup" in message.content.lower():
-            await lookupstripped(message.content, message)
 
 
 async def reminder(command: str, message) -> None:
@@ -65,14 +56,14 @@ async def reminder(command: str, message) -> None:
         if not endtime:
             await message.channel.send(ERRORMESSAGE)
             return
-        delta = pytz.utc.localize(endtime) - now
+        delta = endtime - now
         action = action[:action.rfind("in ")]
     else:
         endtime = dateparser.parse(action[action.rfind("at "):])
         if not endtime:
             await message.channel.send(ERRORMESSAGE)
             return
-        delta = pytz.utc.localize(endtime) - now
+        delta = endtime - now
         action = action[:action.rfind("at ")]
     await reminderoutput(action, delta, message)
 
@@ -158,75 +149,4 @@ async def reminderoutput(action: str, delta: datetime.timedelta, message) -> \
                                "as you requested.")
 
 
-async def lookupstripped(command: str, message) -> None:
-    """
-    Processes command-style input for the lookup function.
-    Format: Hey bot/[mention] lookup [item]
-    Currently not supporting lookup of Excalibur.
-    """
-    item = command[command.lower().rfind("lookup") + 7:]
-    if item == "Excalibur":
-        await message.channel.send("GBF lookup currently does not support "
-                                   "lookup of Excalibur")
-        return
-    await lookupoutput(item, message)
-
-
-async def lookupoutput(item: str, message) -> None:
-    url = "https://gbf.wiki/" + item.replace(" ", "_")
-    page = requests.get(url)
-    if "There is currently no text in this page." in page.text:
-        await message.channel.send("Could not find this item. Search "
-                                   "functionality will be added soon.")
-        return
-        # page = requests.get("https://gbf.wiki/index.php?search=" +
-        #                     item.replace(" ", "+"))
-        # results = [i for i in range(len(page.text)) if page.text.startswith
-        #            ("mw-search-result-heading", i)]
-    categoryindex = page.text.find("wgCategories")
-    if "Weapons" not in page.text[categoryindex + 25:].split("]", 1)[0]:
-        await message.channel.send("This is not a weapon page. I can't handle "
-                                   "non-weapon pages right now.")
-        return
-    description = page.text[page.text.find
-                            ("meta name=\"description\" content=") + 33:]\
-        .split('"', 1)[0]
-    obtainraw = page.text[page.text.find("class=\"obtain-list\"><div>") + 25:]\
-        .split("</div>", 1)[0]
-    obtainlinks = [i for i in range(len(obtainraw)) if obtainraw.startswith
-                   ("<a href=", i)]
-    obtaintext = [i for i in range(len(obtainraw)) if obtainraw.startswith
-                  ("\">", i)]
-    index = 0
-    while index < len(obtaintext) and obtaintext[index] < len(obtainraw):
-        if obtainraw[obtaintext[index] + 2: obtaintext[index] + 6] == "<img":
-            del obtainlinks[index]
-            del obtaintext[index]
-        index += 1
-    obtain = ""
-    i = 0
-    while i < len(obtainlinks):
-        obtain += ("[" + obtainraw[obtaintext[i] + 2:].split('<', 1)[0] +
-                   "](https://gbf.wiki" +
-                   obtainraw[obtainlinks[i] + 9:].split('"', 1)[0] + ")\n")
-        i += 1
-    image = page.text[page.text.find("flex-direction:row;\">") + 21:].split(
-        "</a>", 1)[0]
-    image = "https://gbf.wiki" + \
-            image[image.find("/images/thumb"):].split('"', 1)[0]
-    embed = discord.Embed()
-    embed.title = page.text[page.text.find("wgTitle") + 10:].split('"', 1)[0]
-    embed.url = url
-    embed.set_author(name="GBF Wiki Lookup",
-                     icon_url="https://gbf.wiki/images/1/18/Vyrnball.png?0704c")
-    embed.description = description
-    embed.add_field(name="Obtain", value=obtain)
-    embed.set_thumbnail(url=image)
-    embed.set_footer(text="Brynhildr Bot is not associated with the GBF Wiki.")
-    try:
-        await message.channel.send(embed=embed)
-    except:
-        await message.channel.send("Something went wrong. Please let the bot"
-                                   " owner know so this can be fixed.")
-
-client.run("NzI5NzkwNDYwMTc1ODQzMzY4.XwON_A.sXcW5jkXUSr3o3jvRTXXljBvZzg")
+client.run("NzI5MzkyNDIwNzA1NDAzMDEw.XwO0Ig.Y4om2skeY3Aoqfx0MZp5B27sdqM")
