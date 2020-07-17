57a7f900c51c28a2a87fee9cf32f2c89c95224be
vic-yush <66185358+vic-yush@users.noreply.github.com>
2020-07-17 14:58:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -42,10 +42,8 @@ async def on_message(message):
             await reminder(message.content.lower(), message)
         elif "remindme" in message.content.lower():
             await reminderstripped(message.content.lower(), message)
-        elif any(ele in message.content.lower for ele in GBF):
-            await lookup(message.content, message, "GBF")
-        elif any(ele in message.content.lower for ele in LEAGUE):
-            await lookup(message.content, message, "LOL")
+        elif "lookup" in message.content.lower():
+            await lookup(message.content, message)
         elif "help" in message.content.lower():
             await manual(message)
         elif "changelog" in message.content.lower():
@@ -294,7 +292,7 @@ async def reminderoutput(action: str, delta: datetime.timedelta, message) -> \
                                "as you requested.")
 
 
-async def lookup(command: str, message, cat: str) -> None:
+async def lookup(command: str, message) -> None:
     """
     Processes command-style input for the lookup function.
     Format: Hey bot/[mention] lookup [item]
@@ -303,8 +301,7 @@ async def lookup(command: str, message, cat: str) -> None:
         item = command[command.lower().rfind("lookup") + 10:]
     else:
         item = command[command.lower().rfind("look up") + 11:]
-    if cat == "GBF":
-        await lookupgbf(item, message)
+    lookupgbf(item, message)
 
 
 async def lookupgbf(item: str, message) -> None:
